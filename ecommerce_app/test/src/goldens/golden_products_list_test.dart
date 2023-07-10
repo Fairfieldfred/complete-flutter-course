b0eb50937ccdda0f66a6adabc040e811908f0d66
Fred Blair <fredblair@att.net>
2023-07-09 17:06:17 -0700

    lesson 107

--- ecommerce_app/test/src/goldens/golden_products_list_test.dart
+++ ecommerce_app/test/src/goldens/golden_products_list_test.dart
@@ -1,19 +1,30 @@
+import 'dart:ui';
+
 import 'package:ecommerce_app/src/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../robot.dart';
 
 void main() {
+  final sizeVariant = ValueVariant<Size>({
+    const Size(300,300),
+    const Size(600, 600),
+    const Size(1000, 1000)
+  });
   testWidgets('Golden - products list', (tester) async {
     final r = Robot(tester);
+    final currentSize = sizeVariant.currentValue!;
+    await r.golden.setSurfaceSize(currentSize);
     await r.golden.loadRobotoFont();
     await r.golden.loadMaterialIconFont();
     await r.pumpMyApp();
     await r.golden.precacheImages();
     await expectLater(
       find.byType(MyApp),
-      matchesGoldenFile('producst_list.png',
+      matchesGoldenFile('producst_list_${currentSize.width.toInt()}x${currentSize.height.toInt()}.png',
       )
     );
-  });
+  },
+   variant: sizeVariant,
+   tags: ['golden']);
 }
